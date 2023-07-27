888d0325fa3e84875cd9541b5630cb2d1301edf3
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-07-27 22:40:54 +0200

    Add path to programs

--- clicker/main.py
+++ clicker/main.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from menu import show_menu
 
+CUR_DIR = Path.cwd()
 
 print(' Welcome in Clicker! '.center(50, '='))
 while True:
@@ -9,9 +11,14 @@ while True:
     user_choice = input('>>> ')
 
     if user_choice == '1':
+        folder = "clicker"
+        folder_name = "programs"
+        folder_path = Path.joinpath(CUR_DIR, folder)
+        folder_path = Path.joinpath(folder_path, folder_name)
+
         submenu = options[int(user_choice)-1]
         message = 'Which clicker do you want to run?'
-        options = [1, 2]
+        options = [file_name.stem for file_name in Path.iterdir(folder_path)]
         print(show_menu(options, submenu, end_message=message))
     else:
         break
