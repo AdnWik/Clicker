23deb7bb0e58f42142a9ce40191e4c358bd4b5a9
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-08-07 21:31:03 +0200

    Add docstring to helper

--- clicker/helper.py
+++ clicker/helper.py
@@ -3,6 +3,14 @@ from pynput import mouse
 
 
 def on_click(x, y, button, pressed):
+    """Print mouse position on click
+
+    Args:
+        x (_type_): position x in pixels
+        y (_type_): position y in pixels
+        button (_type_): _description_
+        pressed (_type_): _description_
+    """
     print(f'x: {x} y: {y}')
 
 
